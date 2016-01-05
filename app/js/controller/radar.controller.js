34a3c15651dec0745fea76ad4da5ee0fe4fdea69
solar-dev23 <solar.dev023@gmail.com>
2016-01-05 15:33:16 +0100

    Update - radar page.

--- app/js/controller/radar.controller.js
+++ app/js/controller/radar.controller.js
@@ -83,7 +83,6 @@
             //---------------------------------------------------------//
             // Load Single Page of Tiles
             //--------------------------------------------------------//
-console.log($scope.inMotion, TileService.moreRadar());
             if ( $scope.inMotion || ! TileService.moreRadar() ) {
                 //---------------------------------------------------------------
                 // Check Cache Size of Controller if navigation has left the View
