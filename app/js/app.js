6ff49b6e3cec1c81230d42c7dfef47ba2ef87a76
solar-dev23 <solar.dev023@gmail.com>
2016-01-26 14:03:34 +0100

    Update - Circle Slider.

--- app/js/app.js
+++ app/js/app.js
@@ -60,16 +60,17 @@
             'app.club-detail',
             'app.tile-image',
             'app.tile-video',
-            'app.tile-videoList'
+            'app.tile-videoList',
+            'app.circle-slider'
         ])
         .constant('APP_APIS', {
-          'base':                 'http://demo.data.yabrfish.com/yfapi',
-          'commerce':             'http://demo.data.yabrfish.com/yfapi/commerceservice',
-          'tile':                 'http://demo.data.yabrfish.com/yfapi/tileservice',
-          'lookup':               'http://demo.data.yabrfish.com/yfapi/lookupservice',
-          'reco':                 'http://demo.data.yabrfish.com/yfapi/recoservice',
-          'viewer':               'http://demo.data.yabrfish.com/yfapi/viewerservice',
-          'media':                'http://demo.data.yabrfish.com/yfapi/mediaservice'
+          'base':                 'http://data.yabrfish.com/yfapi',
+          'commerce':             'http://data.yabrfish.com/yfapi/commerceservice',
+          'tile':                 'http://data.yabrfish.com/yfapi/tileservice',
+          'lookup':               'http://data.yabrfish.com/yfapi/lookupservice',
+          'reco':                 'http://data.yabrfish.com/yfapi/recoservice',
+          'viewer':               'http://data.yabrfish.com/yfapi/viewerservice',
+          'media':                'http://data.yabrfish.com/yfapi/mediaservice'
           // 'media':                'http://data.yabrfish.com:9097/mediaservice'
         });
 })();
