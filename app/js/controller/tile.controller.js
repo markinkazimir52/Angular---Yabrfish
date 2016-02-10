e8ddc47e31e318fc89f637495db64fa2be231e91
solar-dev23 <solar.dev023@gmail.com>
2016-02-10 12:09:48 +0100

    Update - Tile Detail Dialog.

--- app/js/controller/tile.controller.js
+++ app/js/controller/tile.controller.js
@@ -7,46 +7,46 @@
     'use strict';
 
     angular
-		.module('app.tile', ["com.2fdevs.videogular", "com.2fdevs.videogular.plugins.controls", "info.vietnamcode.nampnq.videogular.plugins.youtube", 'ngMap', 'flash'])
-		.directive('sycovideo', function(){
-            return {
-              restrict: 'E',
-              template: '<div id="{{id}}" class="player"></div>',
-              scope: {
-                hls_source: "=hls",
-                id: "=id"
-              },
-              link: function(scope, elm, attr) {
-                scope.$on('linkChanged', function(event, val, id) {
-                  if(scope.id == id) {
-                    angular.element('.player').each(function(){
-                      var vid = angular.element(this).attr('id');
-                      bitdash(vid).destroy();                      
-                    });
-                    if(val) {
-                      var conf = {
-                          key:       '9dfc435e221ba94fd0cdbacda4c656cf',
-                          playback: {
-                            autoplay : true
-                          },
-                          source: {
-                            hls: val,
-                          },
-                          events: {
-                            onReady : function(data) {
-                            }                          
-                          }
-                      };
-                      bitdash(scope.id).setup(conf);
-                    }
-                  }
-                });
-              }
-            }
-        })
+		.module('app.tile', ["com.2fdevs.videogular", "com.2fdevs.videogular.plugins.controls", "info.vietnamcode.nampnq.videogular.plugins.youtube", 'ngMap', 'flash', 'ngDialog'])
+		// .directive('sycovideo', function(){
+  //           return {
+  //             restrict: 'E',
+  //             template: '<div id="{{id}}" class="player"></div>',
+  //             scope: {
+  //               hls_source: "=hls",
+  //               id: "=id"
+  //             },
+  //             link: function(scope, elm, attr) {
+  //               scope.$on('linkChanged', function(event, val, id) {
+  //                 if(scope.id == id) {
+  //                   angular.element('.player').each(function(){
+  //                     var vid = angular.element(this).attr('id');
+  //                     bitdash(vid).destroy();                      
+  //                   });
+  //                   if(val) {
+  //                     var conf = {
+  //                         key:       '9dfc435e221ba94fd0cdbacda4c656cf',
+  //                         playback: {
+  //                           autoplay : true
+  //                         },
+  //                         source: {
+  //                           hls: val,
+  //                         },
+  //                         events: {
+  //                           onReady : function(data) {
+  //                           }                          
+  //                         }
+  //                     };
+  //                     bitdash(scope.id).setup(conf);
+  //                   }
+  //                 }
+  //               });
+  //             }
+  //           }
+  //       })
 		.controller('tileController', tileController);
 
-	function tileController($rootScope, $scope, $http, $sce, RouteHelpers, APP_APIS, TileService, ViewerService, Flash) {
+	function tileController($rootScope, $scope, $http, $sce, RouteHelpers, APP_APIS, TileService, ViewerService, Flash, ngDialog) {
 
 		$rootScope.youtubePlay = false;
 
@@ -145,138 +145,6 @@
 			}
 		}
 
-		// $scope.getVideoList = function(element){
-
-		// 	var uid = element.externalId;
-		// 	$scope.loading = true;
-		// 	element.videoList = [];
-		// 	element.videoTitles = [];
-		// 	element.videoImages = [];
-		// 	element.videoType = '';
-
-		// 	element.youtube = {
-		// 		config: {}
-		// 	}
-
-		// 	var video_list = angular.element("#tile_"+element.externalId+ " .video-list");
-		// 	if(video_list[0])
-		// 		video_list[0].style.display = 'block';
-
-		// 	$http.get(APP_APIS['tile']+'/tiles/' + uid + '/content')
-		// 		.success(function(data){
-		// 			if(data.contentList && data.contentList.length>0){
-		// 				element.videoType = data.contentList[0].externalRefs[0].providerCode.toLowerCase();
-		// 				if(element.videoType == 'youtube'){
-		// 					element.vid = data.contentList[0].externalRefs[0].externalContentId;
-		// 					for( var i in data.contentList ){
-		// 						element.videoTitles[i] = data.contentList[i].title;
-		// 					}
-		// 				}else if (element.videoType == 'syco') {
-		// 					for( var i = 0; i < data.contentList.length; i++ ){
-		// 						var vid = data.contentList[i].externalRefs[0].externalContentId;
-		// 						$http.get('http://api1.syndicatecontent.com/Sc.Content.Api.External/ScContentExt/inventory/'+vid+'?mediaformatid=9&vendortoken=B9C333B9-54F3-40B6-8C34-7A6512955B98')
-		// 							.success(function(data) {
-		// 								if(data.resources[0].medias[0].hostId){
-		// 									element.videoList.push(data.resources[0].medias[0]);
-		// 								}
-		// 							});
-		// 						element.videoTitles.push(data.contentList[i].title);
-		// 						element.videoImages.push(data.contentList[i].creatives[0].url);
-		// 					}
-		// 				}
-		// 			}
-		// 			$scope.loading = false;
-		// 		})
-		// }
-
-   //      $scope.videoPlay = function(element, video) {
-			// $rootScope.youtubePlay = false;
-			// if(element.videoType == 'syco'){
-			// 	element.hls_source = video.clientPlayBackUrl + video.url;
-			// 	$rootScope.$broadcast( "linkChanged", element.hls_source, element.externalId);
-			// }
-
-			// var tileVideo = angular.element("#tile_"+element.externalId+ " .tileVideo");
-			// var tileImg = angular.element("#tile_"+element.externalId+ " .tileImg");
-			// var video_list = angular.element("#tile_"+element.externalId+ " .video-list");
-			// var ribbon = angular.element("#tile_"+element.externalId+ " .ribbon");
-
-			// tileVideo.height(tileImg.height());
-
-			// angular.element(".tileImg").css('display', 'inline-block');
-			// angular.element(".tileVideo").css('display', 'none');
-			// angular.element(".youtubeVideo-wrap").css('display', 'none');        
-			// angular.element(".video-list").css('display', 'none');
-
-			// tileVideo[0].style.display = 'inline-block';
-			// tileImg[0].style.display = 'none';
-			// video_list[0].style.display = 'block';
-
-			// if(ribbon[0])
-			// 	ribbon[0].style.display = 'none';
-   //      }
-
-   //      $scope.youtubeVideoPlay = function(element) {
-			// element.showYoutube = true;
-			// $rootScope.youtubePlay = true;
-			// $rootScope.$broadcast( "linkChanged", element.hls_source, element.externalId);
-
-			// element.youtube.config = {
-			// 	preload: "none",
-			// 	autoPlay: true,
-			// 	sources: [
-			// 		{src: "https://www.youtube.com/watch?v=" + element.vid},
-			// 		{src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.webm"), type: "video/webm"},
-			// 		{src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.ogg"), type: "video/ogg"}
-			// 	],
-			// 	theme: {
-			// 		url: "http://www.videogular.com/styles/themes/default/latest/videogular.css"
-			// 	},
-			// 	plugins: {
-			// 		controls: {
-			// 			autoHide: true,
-			// 			autoHideTime: 5000
-			// 		}
-			// 	}
-			// };
-
-			// var youtubeVideo = angular.element("#tile_"+element.externalId+ " .youtubeVideo-wrap");
-			// var tileImg = angular.element("#tile_"+element.externalId+ " .tileImg");
-			// var video_list = angular.element("#tile_"+element.externalId+ " .video-list");
-			// var ribbon = angular.element("#tile_"+element.externalId+ " .ribbon");
-
-			// youtubeVideo.height(tileImg.height());
-
-			// angular.element(".tileImg").css('display', 'inline-block');
-			// angular.element(".tileVideo").css('display', 'none');
-			// angular.element(".youtubeVideo-wrap").css('display', 'none');
-			// angular.element(".video-list").css('display', 'none');
-
-			// youtubeVideo[0].style.display = 'inline-block';
-			// tileImg[0].style.display = 'none';
-			// video_list[0].style.display = 'block';
-
-			// if(ribbon[0])
-			// 	ribbon[0].style.display = 'none';
-   //      }
-
-   //      $scope.hideVideo = function(element) {
-			// $rootScope.$broadcast( "linkChanged", element.hls_source, element.externalId);
-			// $rootScope.youtubePlay = false;
-
-			// var tileVideo = angular.element("#tile_"+element.externalId+ " .tileVideo");
-			// var youtubeVideo = angular.element("#tile_"+element.externalId+ " .youtubeVideo-wrap");
-			// var tileImg = angular.element("#tile_"+element.externalId+ " .tileImg");
-			// var ribbon = angular.element("#tile_"+element.externalId+ " .ribbon");
-
-			// tileVideo[0].style.display = 'none';
-			// youtubeVideo[0].style.display = 'none';
-			// tileImg[0].style.display = 'inline-block';
-		
-			// if(ribbon[0])
-			// 	ribbon[0].style.display = 'inline-block';
-   //      }
-
 		$scope.getTileEvents = function(element) {
 			console.log("Events Logging")
 		}
@@ -322,27 +190,22 @@
             }
         }
 
-        // For Circular Slider Testing. - Ryan(2016.1.23)
-   //      $scope.getEvents = function(element) {	        
-			// TileService.getFirstEvent(element.externalId).then(function(event){				
-			// 	var eventId = event.externalId;
-
-			// 	TileService.getClasses(eventId).then(function(classes){
-   //  				element.classes = classes;
-   //  				for(var i in element.classes) {
-   //                      var flag = "http://img.yabrfish.com/cdn/flags/"+element.classes[i].classFlag.toLowerCase()+".jpg";
-   //                      element.classes[i].flag = flag;
-   //                  }
-   //  			}, function(error){
-   //  				console.log(error);
-   //  				return;
-   //  			})
-			// })
-   //      }
-
         $scope.$on('results', function(e, data){
-console.log(data);        	
 			$scope.results = data;
         })
+
+
+      	$scope.openTileMore = function (tile) {
+      		$scope.tile = tile;
+      		
+      		if(tile.tileType == 'event'){
+				ngDialog.open({ 
+					template: 'app/views/tile-detail-modal.html',
+					className: 'ngdialog-theme-tile-detail',
+					controller: 'eventController',
+					scope: $scope
+				});
+      		}
+		}
     }
 })();
